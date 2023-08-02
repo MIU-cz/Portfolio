9505babb2245cceaacdcc121c0ba7744f7ddb0c4
MIU-cz <0151300@gmail.com>
2023-08-02 20:08:09 +0200

    fix: change links again 🤯

--- loopstudios-Vue/new-loopstudios/vue.config.js
+++ loopstudios-Vue/new-loopstudios/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Portfolio/new-loopstudios/dist/'
+    ? '/Portfolio/loopstudios-Vue/new-loopstudios/dist/'
     : './',
 });
